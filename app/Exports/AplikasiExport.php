7cbc6bd699cc2f595730675e89063b114a4691d9
frost850 <farhan.dewanata22@gmail.com>
2025-01-13 08:51:48 +0700

    export fixed

--- app/Exports/AplikasiExport.php
+++ app/Exports/AplikasiExport.php
@@ -3,30 +3,85 @@
 namespace App\Exports;
 
 use App\Models\Aplikasi;
+use App\Models\AtributTambahan;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Facades\Log;
 
 class AplikasiExport implements FromCollection, WithHeadings
 {
-    public function collection()
+    protected $uniqueAtributs;
+
+    public function __construct()
     {
-        return Aplikasi::select('nama', 'opd', 'uraian', 'tahun_pembuatan', 'jenis', 'basis_aplikasi', 'bahasa_framework', 'database','pengembang','lokasi_server','status_pemakaian')->get();
+        // Ambil semua nama atribut unik
+        $this->uniqueAtributs = AtributTambahan::select('nama_atribut')
+            ->distinct()
+            ->pluck('nama_atribut');
     }
 
     public function headings(): array
     {
-        return [
+        // Kolom dasar
+        $headers = [
             'Nama',
             'OPD',
             'Uraian',
             'Tahun Pembuatan',
             'Jenis',
             'Basis Aplikasi',
-            'Bahasa Framework',
+            'Bahasa/Framework',
             'Database',
             'Pengembang',
             'Lokasi Server',
             'Status Pemakaian'
         ];
+
+        // Tambahkan atribut dinamis ke header
+        foreach ($this->uniqueAtributs as $atribut) {
+            $headers[] = $atribut;
+        }
+
+        return $headers;
     }
-} 
+
+    public function collection()
+    {
+        try {
+            Log::info('Fetching data for export');
+            
+            $aplikasis = Aplikasi::with('atributs')->get();
+            
+            return $aplikasis->map(function ($aplikasi) {
+                // Data dasar
+                $row = [
+                    $aplikasi->nama,
+                    $aplikasi->opd,
+                    $aplikasi->uraian,
+                    $aplikasi->tahun_pembuatan,
+                    $aplikasi->jenis,
+                    $aplikasi->basis_aplikasi,
+                    $aplikasi->bahasa_framework,
+                    $aplikasi->database,
+                    $aplikasi->pengembang,
+                    $aplikasi->lokasi_server,
+                    $aplikasi->status_pemakaian,
+                ];
+
+                // Tambahkan nilai atribut dinamis
+                foreach ($this->uniqueAtributs as $atributName) {
+                    $atribut = $aplikasi->atributs
+                        ->where('nama_atribut', $atributName)
+                        ->first();
+                    
+                    $row[] = $atribut ? $atribut->nilai_atribut : '-';
+                }
+
+                return $row;
+            });
+        } catch (\Exception $e) {
+            Log::error('Error in AplikasiExport collection: ' . $e->getMessage());
+            throw $e;
+        }
+    }
+}
